f1f470c000910c556ce4d89934aa265ade100bc9
Ridwan <ridwanediallo99@gmail.com>
2022-03-22 13:06:55 +0000

    Import core-js and regenerator-runtime

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,6 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const icons = new URL('../img/icons.svg', import.meta.url);
 
 const recipeContainer = document.querySelector('.recipe');
