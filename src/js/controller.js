67340c9f3086ca91af041a0445560bde32b0f220
Ridwan <ridwanediallo99@gmail.com>
2022-03-22 12:54:33 +0000

    Fix icons not displaying

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,5 @@
+const icons = new URL('../img/icons.svg', import.meta.url);
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -39,7 +41,9 @@ const showRecipe = async function () {
 
     const html = `
     <figure class="recipe__fig">
-    <img src="${recipe.image}" crossOrigin = "anonymous " alt="${recipe.title}" class="recipe__img" />
+    <img src="${recipe.image}" crossOrigin = "anonymous " alt="${
+      recipe.title
+    }" class="recipe__img" />
     <h1 class="recipe__title">
       <span>${recipe.title}</span>
     </h1>
@@ -48,27 +52,31 @@ const showRecipe = async function () {
   <div class="recipe__details">
     <div class="recipe__info">
       <svg class="recipe__info-icon">
-        <use href="src/img/icons.svg#icon-clock"></use>
+        <use href="${icons.href}#icon-clock"></use>
       </svg>
-      <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
+      <span class="recipe__info-data recipe__info-data--minutes">${
+        recipe.cookingTime
+      }</span>
       <span class="recipe__info-text">minutes</span>
     </div>
     <div class="recipe__info">
       <svg class="recipe__info-icon">
-        <use href="src/img/icons.svg#icon-users"></use>
+        <use href="${icons.href}#icon-users"></use>
       </svg>
-      <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+      <span class="recipe__info-data recipe__info-data--people">${
+        recipe.servings
+      }</span>
       <span class="recipe__info-text">servings</span>
 
       <div class="recipe__info-buttons">
         <button class="btn--tiny btn--increase-servings">
           <svg>
-            <use href="src/img/icons.svg#icon-minus-circle"></use>
+            <use href="${icons.href}#icon-minus-circle"></use>
           </svg>
         </button>
         <button class="btn--tiny btn--increase-servings">
           <svg>
-            <use href="src/img/icons.svg#icon-plus-circle"></use>
+            <use href="${icons.href}#icon-plus-circle"></use>
           </svg>
         </button>
       </div>
@@ -76,12 +84,12 @@ const showRecipe = async function () {
 
     <div class="recipe__user-generated">
       <svg>
-        <use href="src/img/icons.svg#icon-user"></use>
+        <use href="${icons.href}#icon-user"></use>
       </svg>
     </div>
     <button class="btn--round">
       <svg class="">
-        <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+        <use href="${icons.href}#icon-bookmark-fill"></use>
       </svg>
     </button>
   </div>
@@ -89,11 +97,12 @@ const showRecipe = async function () {
   <div class="recipe__ingredients">
     <h2 class="heading--2">Recipe ingredients</h2>
     <ul class="recipe__ingredient-list">
-    ${recipe.ingredients.map(ing => {
-      return `
+    ${recipe.ingredients
+      .map((ing) => {
+        return `
           <li class="recipe__ingredient">
           <svg class="recipe__icon">
-            <use href="src/img/icons.svg#icon-check"></use>
+            <use href="${icons.href}#icon-check"></use>
           </svg>
           <div class="recipe__quantity">${ing.quantity}</div>
           <div class="recipe__description">
@@ -101,8 +110,9 @@ const showRecipe = async function () {
             ${ing.description}
           </div>
         </li>
-      `
-    }).join('')}
+      `;
+      })
+      .join('')}
     
     </ul>
   </div>
@@ -111,7 +121,9 @@ const showRecipe = async function () {
     <h2 class="heading--2">How to cook it</h2>
     <p class="recipe__directions-text">
       This recipe was carefully designed and tested by
-      <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+      <span class="recipe__publisher">${
+        recipe.publisher
+      }</span>. Please check out
       directions at their website.
     </p>
     <a
@@ -121,13 +133,13 @@ const showRecipe = async function () {
     >
       <span>Directions</span>
       <svg class="search__icon">
-        <use href="src/img/icons.svg#icon-arrow-right"></use>
+        <use href="${icons.href}#icon-arrow-right"></use>
       </svg>
     </a>
   </div>
     `;
     recipeContainer.innerHTML = '';
-    recipeContainer.insertAdjacentHTML('afterbegin', html)
+    recipeContainer.insertAdjacentHTML('afterbegin', html);
   } catch (err) {
     alert(err);
   }
