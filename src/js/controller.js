18d30b4c638aa3a10d20a685f773300e3bc1068d
Ridwan <ridwanediallo99@gmail.com>
2022-03-23 13:12:07 +0000

    Move all that are related to rendering something to the UI into recipeView class

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,9 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import * as modal from './modal.js';
+import recipeView from './views/recipeView.js';
 
 
-const icons = new URL('../img/icons.svg', import.meta.url);
 
 const recipeContainer = document.querySelector('.recipe');
 
@@ -19,135 +19,26 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
-const renderSpinner = function (parentEl) {
-  const markup = ` 
-  <div class="spinner">
-  <svg>
-    <use href="${icons.href}#icon-loader"></use>
-  </svg>
-</div>
-    `;
-  parentEl.innerHTML = '';
-  parentEl.insertAdjacentHTML('afterbegin', markup);
-};
 
-const showRecipe = async function () {
-  renderSpinner(recipeContainer);
+
+const controlRecipe = async function () {
+  recipeView.renderSpinner();
   try {
     const id = window.location.hash.slice(1);
     if (!id) return;
     // 1.Loading the recipe
      await modal.loadRecipe(id);
-  const {recipe} = modal.state;
 
     // 2. Rendering the recipe
 
-    const html = `
-    <figure class="recipe__fig">
-    <img src="${recipe.image}" crossOrigin = "anonymous " alt="${
-      recipe.title
-    }" class="recipe__img" />
-    <h1 class="recipe__title">
-      <span>${recipe.title}</span>
-    </h1>
-  </figure>
+    recipeView.render(modal.state.recipe)
 
-  <div class="recipe__details">
-    <div class="recipe__info">
-      <svg class="recipe__info-icon">
-        <use href="${icons.href}#icon-clock"></use>
-      </svg>
-      <span class="recipe__info-data recipe__info-data--minutes">${
-        recipe.cookingTime
-      }</span>
-      <span class="recipe__info-text">minutes</span>
-    </div>
-    <div class="recipe__info">
-      <svg class="recipe__info-icon">
-        <use href="${icons.href}#icon-users"></use>
-      </svg>
-      <span class="recipe__info-data recipe__info-data--people">${
-        recipe.servings
-      }</span>
-      <span class="recipe__info-text">servings</span>
-
-      <div class="recipe__info-buttons">
-        <button class="btn--tiny btn--increase-servings">
-          <svg>
-            <use href="${icons.href}#icon-minus-circle"></use>
-          </svg>
-        </button>
-        <button class="btn--tiny btn--increase-servings">
-          <svg>
-            <use href="${icons.href}#icon-plus-circle"></use>
-          </svg>
-        </button>
-      </div>
-    </div>
-
-    <div class="recipe__user-generated">
-      <svg>
-        <use href="${icons.href}#icon-user"></use>
-      </svg>
-    </div>
-    <button class="btn--round">
-      <svg class="">
-        <use href="${icons.href}#icon-bookmark-fill"></use>
-      </svg>
-    </button>
-  </div>
-
-  <div class="recipe__ingredients">
-    <h2 class="heading--2">Recipe ingredients</h2>
-    <ul class="recipe__ingredient-list">
-    ${recipe.ingredients
-      .map((ing) => {
-        return `
-          <li class="recipe__ingredient">
-          <svg class="recipe__icon">
-            <use href="${icons.href}#icon-check"></use>
-          </svg>
-          <div class="recipe__quantity">${ing.quantity}</div>
-          <div class="recipe__description">
-            <span class="recipe__unit">${ing.unit}</span>
-            ${ing.description}
-          </div>
-        </li>
-      `;
-      })
-      .join('')}
     
-    </ul>
-  </div>
-
-  <div class="recipe__directions">
-    <h2 class="heading--2">How to cook it</h2>
-    <p class="recipe__directions-text">
-      This recipe was carefully designed and tested by
-      <span class="recipe__publisher">${
-        recipe.publisher
-      }</span>. Please check out
-      directions at their website.
-    </p>
-    <a
-      class="btn--small recipe__btn"
-      href="${recipe.sourceUrl}"
-      target="_blank"
-    >
-      <span>Directions</span>
-      <svg class="search__icon">
-        <use href="${icons.href}#icon-arrow-right"></use>
-      </svg>
-    </a>
-  </div>
-    `;
-    recipeContainer.innerHTML = '';
-    recipeContainer.insertAdjacentHTML('afterbegin', html);
   } catch (err) {
     alert(err);
   }
 };
-showRecipe();
+controlRecipe();
 
-['hashchange', 'load'].forEach((ev) => window.addEventListener(ev, showRecipe));
+['hashchange', 'load'].forEach((ev) => window.addEventListener(ev, controlRecipe));
 
